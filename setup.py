26f81e42fae75b48a67dd7b42b0eb4aefb4e35c7
Maksim Rakitin <maxim.rakitin@gmail.com>
2017-12-21 12:07:47 -0500

    ENH: minor updates of setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,18 @@ import versioneer
 
 from setuptools import setup, find_packages
 
+no_git_reqs = []
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+    for r in required:
+        if not (r.startswith('git') or r.startswith('#') or r.strip() == ''):
+            no_git_reqs.append(r)
+
 setup(
     name='chxanalys',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     author='Brookhaven National Laboratory_CHX',
     packages=find_packages(),
-    #package_data={'chxtools/X-ray_database' : ['.dat']},    include_package_data=True,
+    install_requires=no_git_reqs,
 )
