c24cf2b6aca0e7c17e6a935631c23c50cfda5692
=yugang zhang <yuzhang@bnl.gov>
2018-01-17 10:26:36 -0500

    ADD chx_handlers copied from chxtools

---
+++ pyCHX/chx_handlers.py
@@ -0,0 +1,47 @@
+###Copied from chxtools/chxtools/handlers.py
+###https://github.com/NSLS-II-CHX/chxtools/blob/master/chxtools/handlers.py
+
+
+
+# handler registration and database instantiation should be done
+# here and only here!
+from databroker import Broker
+from databroker.assets.handlers_base import HandlerBase
+#from chxtools.pims_readers.eiger import EigerImages
+from eiger_io.fs_handler_dask import EigerHandlerDask, EigerImagesDask
+from eiger_io.fs_handler import EigerHandler as EigerHandlerPIMS, EigerImages as EigerImagesPIMS
+
+
+
+'''
+Tried to allow function to change namespace did not work. 
+DO NOT USE
+'''
+# toggle use of dask or no dask
+# TODO : eventually choose one of the two
+def use_pims(db):
+    global EigerImages, EigerHandler
+    EigerImages = EigerImagesPIMS
+    EigerHandler = EigerHandlerPIMS
+    db.reg.register_handler('AD_EIGER2', EigerHandler, overwrite=True)
+    db.reg.register_handler('AD_EIGER', EigerHandler, overwrite=True)
+
+
+def use_dask(db):
+    global EigerImages, EigerHandler
+
+    EigerImages = EigerImagesDask
+    EigerHandler = EigerHandlerDask
+    db.reg.register_handler('AD_EIGER2', EigerHandler, overwrite=True)
+    db.reg.register_handler('AD_EIGER', EigerHandler, overwrite=True)
+
+# call use_pims or use_dask
+# default is use_dask()
+# TODO : This is hard coded
+# calling this after import won't change things, need to find a better way
+if __name__ == "__main__":
+    db = Broker.named('chx')
+    use_pims(db)
+
+
+
