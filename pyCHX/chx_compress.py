23fcc1e4c175336441b71aa00dd8b5517f90d8d0
=yugang zhang <yuzhang@bnl.gov>
2019-04-02 10:13:47 -0400

    Add MultifileBNL class

--- pyCHX/chx_compress.py
+++ pyCHX/chx_compress.py
@@ -811,6 +811,135 @@ class Multifile_Bins( object  ):
         p=  np.where( x_ ) [0]
         v =  np.array( x_[ p ])
         return ( np.array(p, dtype=np.int32), v)
+    
+    
+class MultifileBNL:
+    '''
+    Re-write multifile from scratch.
+    '''
+    HEADER_SIZE = 1024
+    def __init__(self, filename, mode='rb'):
+        '''
+            Prepare a file for reading or writing.
+            mode : either 'rb' or 'wb'
+        '''
+        if mode == 'wb':
+            raise ValueError("Write mode 'wb' not supported yet")
+        if mode != 'rb' and mode != 'wb':
+            raise ValueError("Error, mode must be 'rb' or 'wb'"
+                             "got : {}".format(mode))
+        self._filename = filename
+        self._mode = mode
+        # open the file descriptor
+        # create a memmap
+        if mode == 'rb':
+            self._fd = np.memmap(filename, dtype='c')
+        elif mode == 'wb':
+            self._fd = open(filename, "wb")
+        # these are only necessary for writing
+        self.md = self._read_main_header()
+        self._cols = int(self.md['nrows'])
+        self._rows = int(self.md['ncols'])
+        # some initialization stuff
+        self.nbytes = self.md['bytes']
+        if (self.nbytes==2):
+            self.valtype = "<i2"#np.uint16
+        elif (self.nbytes == 4):
+            self.valtype = "<i4"#np.uint32
+        elif (self.nbytes == 8):
+            self.valtype = "<i8"#np.float64
+        # frame number currently on
+        self.index()
+    def index(self):
+        ''' Index the file by reading all frame_indexes.
+            For faster later access.
+        '''
+        print('Indexing file...')
+        t1 = time.time()
+        cur = self.HEADER_SIZE
+        file_bytes = len(self._fd)
+        self.frame_indexes = list()
+        while cur < file_bytes:
+            self.frame_indexes.append(cur)
+            # first get dlen, 4 bytes
+            dlen = np.frombuffer(self._fd[cur:cur+4], dtype="<u4")[0]
+            #print("found {} bytes".format(dlen))
+            # self.nbytes is number of bytes per val
+            cur += 4 + dlen*(4+self.nbytes)
+            #break
+        self.Nframes = len(self.frame_indexes)
+        t2 = time.time()
+        print("Done. Took {} secs for {} frames".format(t2-t1, self.Nframes))
+
+    def _read_main_header(self):
+        ''' Read header from current seek position.
+            Extracting the header was written by Yugang Zhang. This is BNL's
+            format.
+            1024 byte header +
+            4 byte dlen + (4 + nbytes)*dlen bytes
+            etc...
+            Format:
+                unsigned int beam_center_x;
+                unsigned int beam_center_y;
+        '''
+        # read in bytes
+        # header is always from zero
+        cur = 0
+        header_raw = self._fd[cur:cur + self.HEADER_SIZE]
+        ms_keys = ['beam_center_x', 'beam_center_y', 'count_time',
+                   'detector_distance', 'frame_time', 'incident_wavelength',
+                   'x_pixel_size', 'y_pixel_size', 'bytes', 'nrows', 'ncols',
+                   'rows_begin', 'rows_end', 'cols_begin', 'cols_end']
+        magic = struct.unpack('@16s', header_raw[:16])
+        md_temp =  struct.unpack('@8d7I916x', header_raw[16:])
+        self.md = dict(zip(ms_keys, md_temp))
+        return self.md
+    def _read_raw(self, n):
+        ''' Read from raw.
+            Reads from current cursor in file.
+        '''
+        if n > self.Nframes:
+            raise KeyError("Error, only {} frames, asked for {}".format(self.Nframes, n))
+        # dlen is 4 bytes
+        cur = self.frame_indexes[n]
+        dlen = np.frombuffer(self._fd[cur:cur+4], dtype="<u4")[0]
+        cur += 4
+        pos = self._fd[cur: cur+dlen*4]
+        cur += dlen*4
+        pos = np.frombuffer(pos, dtype='<u4')
+        # TODO: 2-> nbytes
+        vals = self._fd[cur: cur+dlen*self.nbytes]
+        vals = np.frombuffer(vals, dtype=self.valtype)
+        return pos, vals
+    def rdframe(self, n):
+        # read header then image
+        pos, vals = self._read_raw(n)
+        img = np.zeros((self._rows*self._cols,))
+        img[pos] = vals
+        return img.reshape((self._rows, self._cols))
+    def rdrawframe(self, n):
+        # read header then image
+        return self._read_raw(n)
+    
+class MultifileBNLCustom(MultifileBNL):
+    def __init__(self, filename, beg=0, end=None, **kwargs):
+        super().__init__(filename, **kwargs)
+        self.beg = beg
+        if end is None:
+            end = self.Nframes-1
+        self.end = end
+    def rdframe(self, n):
+        if n > self.end or n < self.beg:
+            raise IndexError("Index out of range")
+        #return super().rdframe(n - self.beg)
+        return super().rdframe(  n  )  
+    def rdrawframe(self, n):
+        #return super().rdrawframe(n - self.beg)
+        if n > self.end or n < self.beg:
+            raise IndexError("Index out of range")        
+        return super().rdrawframe(n )
+    
+    
 
 def get_avg_imgc( FD,  beg=None,end=None, sampling = 100, plot_ = False, bad_frame_list=None,
                  show_progress=True, *argv,**kwargs):   
