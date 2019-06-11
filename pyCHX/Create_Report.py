9a56a24745b3720515d79388740647a1b3b7cf21
=yugang zhang <yuzhang@bnl.gov>
2019-06-11 13:20:45 -0400

    Update trivial

--- pyCHX/Create_Report.py
+++ pyCHX/Create_Report.py
@@ -822,7 +822,8 @@ class create_pdf_report( object ):
                      str2_left, str2_top, return_=True )         
         #print( imgf,self.data_dir )        
         #add g2 plot fit
-        #print(self.q_rate_file)
+        #print('here')
+        #print(self.q_rate_file, q_rate_file)        
         if os.path.isfile( self.q_rate_file ):
             top = top + 70 #
             if q_rate_file is None:
